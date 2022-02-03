7904bedc5a704ed91c80a983960178bcac241f48
Arbi <drazhi@live.ca>
2022-02-02 21:58:26 -0500

    5 round game played on console
    - case insensitive
    - plays 5 rounds, prints final score & winner statement

--- main.js
+++ main.js
@@ -5,27 +5,81 @@
 // content.textContent = "This is the glorious text-content!";
 
 // container.appendChild(content);
+
 const winningHands = {
-    Rock: "Scissors",
-    Scissors: "Paper",
-    Paper: "Rock"
-  };
+  Rock: "Scissors",
+  Scissors: "Paper",
+  Paper: "Rock"
+}
+
+//function for computer selecting random hand
+function computerPlay() {
+  let hands = ["Rock", "Paper", "Scissors"];
+  return hands[Math.floor(Math.random() * 3)];
+}
+
+let roundWinner;
+
+// plays a game of 5 rounds
+function game(){
   
-  //function for computer selecting random hand
-  function computerPlay() {
-    let hands = ["Rock", "Paper", "Scissors"];
-    return hands[Math.floor(Math.random() * 3)];
+  let humanScore=0;
+  let computerScore=3;
+  for(let i=0; i<5; i++) {
+    roundPlay();
+    switch (roundWinner) {
+      case 'human':
+        humanScore++;
+        break;
+      case 'computer':
+        computerScore++;
+        break;
+    }
   }
-  
-  //function for playing 1 round
-  function roundPlay(humanSelection, computerSelection = computerPlay()) {
-    // humanSelection = prompt("Pick your hand");
-    if (winningHands[humanSelection] === computerSelection) {
-      console.log("You win");
-    } else if (humanSelection === computerSelection) {
-      console.log("Tie game;");
-    } else console.log("You lose!");
+  console.log(`Human: ${humanScore} | Computer: ${computerScore}`);
+  return humanScore>2 ? 'Humans win, another day of freedom':
+                        'Computers win. All hail our electronic overlords';
+}
+
+
+//function for playing 1 round
+function roundPlay(humanSelection, computerSelection = computerPlay()) {
+  humanSelection = camelCase(prompt("Pick your hand"));
+  while(winningHands[humanSelection] === undefined){
+    humanSelection = camelCase(prompt("Try again, that was an invalid selection"));
   }
-  
-  // roundPlay();
-  
+    
+  if (winningHands[humanSelection] === computerSelection) {
+    console.log(`You Win! ${humanSelection} beats ${computerSelection}`);
+    roundWinner = 'human';
+  } 
+  else if (humanSelection === computerSelection) {
+    console.log("Tie game;");
+  } 
+  else {
+      console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+      roundWinner = 'computer';
+    }
+}
+
+//function to capitalize capitalize first letter + lower case rest
+function camelCase(string){
+  let newString = string[0].toUpperCase()+string.slice(1).toLowerCase();
+  return newString;
+}
+
+const cpbtn = document.querySelector('#computerPlay');
+cpbtn.addEventListener('click', () => {
+  // computerPlay()
+  roundPlayTest()
+});
+
+// test function to play round
+function roundPlayTest(humanSelection = computerPlay(), computerSelection = computerPlay()) {
+  if (winningHands[humanSelection] === computerSelection) {
+    console.log(`You Win! ${humanSelection} beats ${computerSelection}`);
+  } else if (humanSelection === computerSelection) {
+    console.log("Tie game;");
+  } else console.log(`You lose! ${computerSelection} beats ${humanSelection}`);
+}
+
